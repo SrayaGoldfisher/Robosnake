9a58cebff40012bf366362618831eee19923c3f7
SrayaGoldfisher <47556043+SrayaGoldfisher@users.noreply.github.com>
2019-02-19 12:16:13 +0200

    new rf_reciver

---
+++ RF_Transmitter/RF_Transmitter.ino
@@ -0,0 +1,17 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(7, 8); // CE, CSN
+const byte address[6] = "00001";
+void setup() {
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+}
+void loop() {
+  const int num = 1234;
+  const char text[16] = itoa(num, text, 10);
+  radio.write(&text, sizeof(text));
+  delay(1000);
+}
