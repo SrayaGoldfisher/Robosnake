059cc92a0de8041a8f50fb45ac7bc2f9e3e6f037
avielm <48471199+avielm@users.noreply.github.com>
2019-04-07 12:08:03 +0300

    Update motor_analyze_PID.ino
    now its working

--- motor_analyze_PID.ino
+++ motor_analyze_PID.ino
@@ -6,8 +6,8 @@ long dt = 0;
 long t0 = 0;
 long dt2 = 0;
 long t02 = 0;
-int velo_l = 0;
-int velo_r = 0;
+float velo_l = 0;
+float velo_r = 0;
 long tlop0 = 0;
 int k = 0;
 String inputString = "";
@@ -26,23 +26,23 @@ long t0l;
 float kdl = 0;
 float errorl;
 float errorPRl = 0;
-float Dl;
-float Pl;
-double Il ;
-int ul;
+float Dl = 0;
+float Pl = 0;
+double Il = 0 ;
+int ul = 0;
 float kir = 0;
 float kpr = 0;
-long t0r;
+long t0r = 0;
 float kdr = 0;
-float errorr;
+float errorr = 0;
 float errorPRr = 0;
-float Dr;
-float Pr;
-double Ir ;
-int ur;
+float Dr = 0;
+float Pr = 0;
+double Ir = 0;
+int ur = 0;
 void setup() {
 
-  attachInterrupt(digitalPinToInterrupt(3), en1_1 , RISING);
+  attachInterrupt(digitalPinToInterrupt(7), en1_1 , FALLING);  
   attachInterrupt(digitalPinToInterrupt(25), en2_1 , RISING);
   analogWriteFrequency(5, 5000);
   analogWriteFrequency(6, 5000);
@@ -57,6 +57,8 @@ void setup() {
   pinMode(6, OUTPUT);
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
+  pinMode(8, INPUT);
+  pinMode(32, INPUT);
   analogWrite(5, LOW);
   analogWrite(6, LOW);
   analogWrite(9, LOW);
@@ -78,101 +80,145 @@ void setup() {
   */
 }
 long ts_0 = 0;
-void loop() {
+char p=' ';
+long change = millis();
 
-  if (millis() - ts_0 >= 20)
+void loop() {
+if ( Serial1.available()) {
+    p = Serial1.read();
+
+    if (p == 'l')
+    {
+        while (!Serial1.available());
+      veloln = Serial1.parseFloat();
+      }
+      
+    if (p == 'r')
+    {
+        while (!Serial1.available());
+      velorn = Serial1.parseFloat();
+      }
+    }
+  /*if(Serial1.available())
+    {  veloln = Serial.parseInt();
+    velorn=veloln;}*/
+  /*if (millis() - change >= 20000)
+  { 
+   
+    veloln = veloln+0.1;
+
+    
+    velorn=velorn-0.1;
+  
+    change = millis();
+  }*/
+  if (millis() - ts_0 >= 100)
   {
     ts_0 = millis();
     Serial1.print('l');
 
+    Serial1.print(velo_l);
+    Serial1.print('\n');
+    Serial1.print('r');
+
     Serial1.print(velo_r);
     Serial1.print('\n');
+  Serial.print('l');
+
+   /* Serial.print(velo_l);
+
+    Serial.print('r');
+
+    Serial.print(velo_r);
+ Serial.println('l');
+  */
   }
-
-  kpl = 0.0043;
-  kil = 153.66;
-
-  errorl = veloln - velo_l;
-  Il = Il + errorl;
-  Dl = kdl * (errorl - errorPRl);
-  Pl = kpl * errorl;
-  ul = Pl + Dl / 10 + kil * Il * 0.000002;
-  if (millis() - t0l > 100)
+  if (millis() - t0l > 1)
   {
-    if (ul > 255)
-    { ul = 255;
-      analogWrite(9, ul);
+    kpl = 32509;
+    kil = 100;
+    kdl = 0;
+    errorl = veloln - velo_l;
+    Il = Il + errorl * 0.001;
+    Dl = kdl * (errorl - errorPRl) / 0.001;
+    Pl = kpl * errorl;
+    ul = Pl +  Dl  + kil * Il ;
+    if (ul > 0)
+    { if (ul > 8191)
+        ul = 8191;
       analogWrite(10, LOW);
+      analogWrite(9, (int)ul);
     }
-    else
+    if (ul <= 0)
     {
       ul = -ul;
-      if (ul > 255)
-        ul = 255;
+      if (ul > 8191)
+        ul = 8191;
+      analogWrite(10, (int)ul);
       analogWrite(9, LOW);
-      analogWrite(10, ul);
     }
     errorPRl = errorl;
     t0l = millis();
   }
-  errorPRl = errorl;
   //right
-  kpr = 0.6852;
-  kir = 85.11;
-  kdr = 0.00006412;
-        errorr = velorn - velo_r;
-  Ir = Ir + errorr;
-  Dr = kdr * (errorr - errorPRr);
-  Pr = kpr * errorr;
-  ur = Pr + Dr / 10 + kir * Ir * 0.000002;
-  if (millis() - t0r > 100)
+  if (millis() - t0r > 1)
   {
-    if (ur > 255)
-    { ur = 255;
-      analogWrite(9, ur);
+    kpr = 32509;
+    kir =82;
+    kdr = 0;
+    errorr = velorn - velo_r;
+    Ir = Ir + errorr * 0.001;
+    Dr = kdr * (errorr - errorPRr) / 0.001;
+    Pr = kpr * errorr;
+    ur = (int) Pr + (int)Dr  + (int)kir * (int)Ir  ;
+
+    if (ur > 0)
+    {
+      if (ur > 8191)
+        ur = 8191;
+      analogWrite(5, ur);
+      analogWrite(6, LOW);
+    }
+    if (ur <= 0)
+    { ur = -ur;
+      if (ur > 8191)
+        ur = 8191;
+      analogWrite(5, LOW);
+      analogWrite(6, ur);
+    }
+    t0r = millis();
+    errorPRr = errorr;
+
+  }
+  /*
+    k = 4691 + 3500 * sin(2 * 3.141592 * millis() * 2.5 * 0.001);
+
+    Serial.print(k);
+    if ( (k > 0) && (k < 8191))
+    { analogWrite(5, LOW);
+      analogWrite(6, k);
+      analogWrite(9, LOW);
+      analogWrite(10, k);
+    }
+    if ((k < 0) && (k > -8191))
+    {
+      analogWrite(5, abs(k));
+      analogWrite(6, LOW);
+      analogWrite(9, abs(k));
       analogWrite(10, LOW);
     }
-    else
+    if (k == 0)
     {
-      ur = -ur;
-      if (ur > 255)
-        ur = 255;
+      analogWrite(5, LOW);
+      analogWrite(6, LOW);
       analogWrite(9, LOW);
-      analogWrite(10, ur);
+      analogWrite(10, LOW);
+      velo_r = 0;
+      velo_l = 0;
+      Il = 0;
     }
-    errorPRl = errorl;
-    t0r = millis();
-  }
-  errorPRr = errorr;
 
-  k = 4691 + 3500 * sin(2 * 3.141592 * millis() * 2.5 * 0.001);
-
-  Serial.print(k);
-  if ( (k > 0) && (k < 8191))
-  { analogWrite(5, LOW);
-    analogWrite(6, k);
-    analogWrite(9, LOW);
-    analogWrite(10, k);
-  }
-  if ((k < 0) && (k > -8191))
-  {
-    analogWrite(5, abs(k));
-    analogWrite(6, LOW);
-    analogWrite(9, abs(k));
-    analogWrite(10, LOW);
-  }
-  if (k == 0)
-  {
-    analogWrite(5, LOW);
-    analogWrite(6, LOW);
-    analogWrite(9, LOW);
-    analogWrite(10, LOW);
-    velo_r = 0;
-    velo_l = 0;
-    Il = 0;
-  }
-
-
+  */
 }
 
 
@@ -180,35 +226,35 @@ void loop() {
 
 void en1_1() {
   dt =  micros() - t0;
+  //bool flag = digitalRead(4);
   t0 = micros();
-  if (digitalRead(4) == digitalRead(3))
+  if (!digitalRead(8))
   { counter_1++;
-    velo_r = 2 * 80213.9037 / dt;
-
+    velo_r = 1567.9963333424992990544387652732 / dt;
+    
   }
-  else
+  if (digitalRead(8))
   { counter_1--;
-    velo_r = -2 * 80213.9037 / dt;
-
+    velo_r = -1567.9963333424992990544387652732 / dt;
+   
   }
-
+  
 }
 
 
 void en2_1() {
   dt2 =  micros() - t02;
   t02 = micros();
-  if (digitalRead(25) == digitalRead(32))
+  if (!digitalRead(32))
   { counter_2++;
 
-    velo_l = 2 * 80213.9037 / dt2;
+    velo_l = 1567.9963333424992990544387652732 / dt2;
 
   }
-  else
+  if (digitalRead(32))
+
   { counter_2--;
-    velo_l = -2 * 80213.9037 / dt2;
+    velo_l = -1567.9963333424992990544387652732 / dt2;
 
   }
-
 }
-
