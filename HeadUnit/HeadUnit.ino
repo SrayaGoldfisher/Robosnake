b2c7ecf03c22809977e786a7e57c17ccc07eeaa2
SamYaron <samigol@gmail.com>
2019-05-19 10:41:08 +0300

    IMU עובד

--- HeadUnit/HeadUnit.ino
+++ HeadUnit/HeadUnit.ino
@@ -1,15 +1,22 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <Wire.h>
-#include <VL53L0X.h>
 #include <SPI.h>
 #include "MPU9250.h"
+#include "EEPROM.h"
 
 #define rightMotor 5
 #define rightMotorGND 6
 #define leftMotor 20
 #define leftMotorGND 21
+MPU9250 Imu(Wire, 0x68);
+int status;
 
+// EEPROM buffer and variables to load accel and mag bias
+// and scale factors from CalibrateMPU9250.ino
+uint8_t EepromBuffer[48];
+float axb, axs, ayb, ays, azb, azs;
+float hxb, hxs, hyb, hys, hzb, hzs;
 #define XSHUT_pin4 14
 #define XSHUT_pin2 15
 #define XSHUT_pin1 16
@@ -20,6 +27,7 @@
 //Timer Interrupts PID Control
 IntervalTimer PidTimerLeft;
 IntervalTimer PidTimerRight;
+IntervalTimer PidTimerAngle;
 
 bool IsTurn = 0;
 long Timeofdetaction = 0;
@@ -32,8 +40,8 @@ long dt = 0;
 long t0 = 0;
 long dt2 = 0;
 long t02 = 0;
-float NeededVelocityRightWheelMS = 0.4;
-float NeededVelocityLeftWheelMS = 0.4;
+float NeededVelocityRightWheelMS = 0.5;
+float NeededVelocityLeftWheelMS = 0.5;
 float VelocityRightWheelMS = 0;
 float VelocityLeftWheelMS = 0;
 float KiLeft = 100;
@@ -59,6 +67,36 @@ int URight = 0;
 long ts_0 = 0;
 float StartAngle = 0;
 float Angle = 0;
+float KiAngle = 10;
+float KpAngle = 45;
+float KDAngle = 5 ;
+float ErrorAngle = 0;
+float PreviousErrorAngle = 0;
+float DAngle = 0;
+float PAngle = 0;
+double IAngle = 0;
+int Uheading = 0;
+char SerialIndex = ' ';
+bool begining = 1;
+int RealAngle = 0;
+int RealPitch = 0;
+int StartYaw = 0;
+int StartPitch = 0;
+float NeededAngle = 0;
+
+//gyro read
+double dty = 0;
+double t0y = 0;
+float omegaYaw = 0;
+float thetaYaw = 0;
+float omegaP = 0;
+float thetaP = 0;
+float omegaR = 0;
+float thetaR = 0;
+float omegaZ = 0;
+float omegaY = 0;
+float omegaX = 0;
+float thetaZ = 0;
 void setup() {
   Serial.begin(115200);
   Serial1.begin(115200);
@@ -86,7 +124,44 @@ void setup() {
   analogWrite(rightMotorGND, LOW);
   analogWrite(leftMotor, LOW);
   analogWrite(leftMotorGND, LOW);
+  // start communication with IMU
+  status = Imu.begin();
+  // load and set accel and mag bias and scale
+  // factors from CalibrateMPU9250.ino
+  for (size_t i = 0; i < sizeof(EepromBuffer); i++) {
+    EepromBuffer[i] = EEPROM.read(i);
+  }
+  memcpy(&axb, EepromBuffer + 0, 4);
+  memcpy(&axs, EepromBuffer + 4, 4);
+  memcpy(&ayb, EepromBuffer + 8, 4);
+  memcpy(&ays, EepromBuffer + 12, 4);
+  memcpy(&azb, EepromBuffer + 16, 4);
+  memcpy(&azs, EepromBuffer + 20, 4);
+  memcpy(&hxb, EepromBuffer + 24, 4);
+  memcpy(&hxs, EepromBuffer + 28, 4);
+  memcpy(&hyb, EepromBuffer + 32, 4);
+  memcpy(&hys, EepromBuffer + 36, 4);
+  memcpy(&hzb, EepromBuffer + 40, 4);
+  memcpy(&hzs, EepromBuffer + 44, 4);
 
+  Imu.setAccelCalX(axb, axs);
+  Imu.setAccelCalY(ayb, ays);
+  Imu.setAccelCalZ(azb, azs);
+
+  Imu.setMagCalX(hxb, hxs);
+  Imu.setMagCalY(hyb, hys);
+  Imu.setMagCalZ(hzb, hzs);
+  pinMode(1, OUTPUT);
+
+  // setting a 41 Hz DLPF bandwidth
+  Imu.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_41HZ);
+  // setting SRD to 9 for a 100 Hz update rate
+  Imu.setSrd(9);
+  // enabling the data ready interrupt
+  Imu.enableDataReadyInterrupt();
+  // attaching the interrupt to microcontroller pin 1
+  pinMode(9, INPUT);
+  attachInterrupt(9, yaw, RISING);
   //  Encoder legs
   pinMode(8, INPUT);
   pinMode(23, INPUT);
@@ -94,7 +169,8 @@ void setup() {
   //PID Interrupts
   PidTimerLeft.begin(PidLeft, 1000); //1 millis
   PidTimerRight.begin(PidRight, 1000);
-    Serial.println("start");
+  PidTimerAngle.begin(PidAngle, 1000);
+  Serial.println("start");
   Serial1.println("start");
 
 }
@@ -102,22 +178,39 @@ long TimeRun = millis();
 
 void loop()
 {
-  if(millis() -Timeofdetaction >30){
-  Timeofdetaction=millis();
-  Serial.print(millis());
-  Serial.print("  ");
-  Serial.print(TimeRun);
-  Serial.print("  ");
-  Serial.print(VelocityLeftWheelMS);//NeededVelocityRightWheelMS);
-  Serial.print("  ");
-  Serial.println(ULeft);//NeededVelocityLeftWheelMS);
+  if (millis() - Timeofdetaction > 30) {
+    Timeofdetaction = millis();
+    Serial.print(ErrorAngle);
+    Serial.print("  ");
+    //  Serial.print(TimeRun);
+    //  Serial.print("  ");
+    //  Serial.print(VelocityLeftWheelMS);//NeededVelocityRightWheelMS);
+    //  Serial.print("  ");
+    Serial.println(Uheading);//NeededVelocityLeftWheelMS);
   }
   while ((millis() - TimeRun) <= 10000)
-  { 
+  {
     NeededVelocityLeftWheelMS = 0.5;
     NeededVelocityRightWheelMS = 0.5;
     //Serial.print(NeededVelocityRightWheelMS);
     // Serial.println(NeededVelocityLeftWheelMS);
+    Serial.print(ErrorAngle);
+    Serial.print("  ");
+    Serial.println(Uheading);
+  }
+  while ((millis() - TimeRun) <= 20000)
+  { Serial.print(ErrorAngle);
+    Serial.print("  ");
+    Serial.println(Uheading);
+
+    NeededAngle = 90;
+  }
+  while ((millis() - TimeRun) <= 30000)
+  { Serial.print(ErrorAngle);
+    Serial.print("  ");
+    Serial.println(Uheading);
+
+    NeededAngle = 0;
   }
   NeededVelocityLeftWheelMS = 0;
   NeededVelocityRightWheelMS = 0;
@@ -125,10 +218,10 @@ void loop()
 void PidLeft()
 {
   ErrorLeft = NeededVelocityLeftWheelMS - VelocityLeftWheelMS;
-  ILeft = (ILeft + ErrorLeft * 0.001)*(NeededVelocityLeftWheelMS!=0);
+  ILeft = (ILeft + ErrorLeft * 0.001) * (NeededVelocityLeftWheelMS != 0);
   DLeft = KdLeft * (ErrorLeft - PreviousErrorLeft) / 0.001;
   PLeft = KpLeft * ErrorLeft;
-  ULeft = PLeft +  DLeft + KiLeft * ILeft ;
+  ULeft = PLeft +  DLeft + KiLeft * ILeft + Uheading ;
   if (ULeft >= 0)
   { if (ULeft > 8191)
       ULeft = 8191;
@@ -150,10 +243,10 @@ void PidLeft()
 void PidRight()
 {
   ErrorRight = NeededVelocityRightWheelMS - VelocityRightWheelMS;
-  IRight = (IRight + ErrorRight * 0.001)*(NeededVelocityRightWheelMS!=0);
+  IRight = (IRight + ErrorRight * 0.001) * (NeededVelocityRightWheelMS != 0);
   DRight = KdRight * (ErrorRight - PreviousErrorRight) / 0.001;
   PRight = KpRight * ErrorRight;
-  URight =  PRight + DRight  + KiRight * IRight  ;
+  URight =  PRight + DRight  + KiRight * IRight - Uheading  ;
 
   if (URight > 0)
   {
@@ -171,7 +264,17 @@ void PidRight()
   }
   PreviousErrorRight = ErrorRight;
 }
+void PidAngle()
+{ //(int)theta//RealAngle//
+  ErrorAngle = NeededAngle - RealAngle;
+  IAngle = (IAngle + ErrorAngle * 0.001) * (ErrorAngle != 0);
+  DAngle = KDAngle * (ErrorAngle - PreviousErrorAngle) / 0.001;
+  PAngle = KpAngle * ErrorAngle;
+  Uheading =  PAngle + DAngle  + KiAngle * IAngle  ;
 
+  PreviousErrorAngle = ErrorAngle;
+
+}
 
 void en1_1() {
   dt =  micros() - t0;
@@ -206,4 +309,27 @@ void en2_1() {
     VelocityLeftWheelMS = 1567.9963333424992990544387652732 / dt2;
 
   }
+}
+void yaw()
+{ Imu.readSensor();
+  dty =  micros() - t0y;
+  t0y = micros();
+  //  begining = 0;
+  //local omega
+  omegaYaw = Imu.getGyroZ_rads() ;//* (180.0f / PI);
+  omegaP = Imu.getGyroX_rads();// * (180.0f / PI);
+  omegaR = Imu.getGyroY_rads() ;//* (180.0f / PI);
+  thetaP = thetaP + omegaP * (dty / 1000000);
+  thetaYaw = thetaYaw + omegaYaw * (dty / 1000000);
+  thetaR = thetaR + omegaR * (dty / 1000000);
+  //world omega
+  omegaZ = (cos(thetaP) * cos(thetaR) * omegaYaw + sin(thetaP) * cos(thetaR) * omegaR - omegaP * sin(thetaR)) * (180.0f / PI);
+  omegaX = (cos(thetaYaw) * omegaP - sin(thetaYaw) * omegaR) * (180.0f / PI);
+  omegaY = (sin(thetaYaw) * omegaP + cos(thetaYaw) * omegaR) * (180.0f / PI);
+
+  //  thetaY = thetaY + omegaY * (dty / 1000000);
+  //   thetaP = thetaP + omegaP * (dty / 1000000);
+  //  thetaR = thetaR + omegaR * (dty / 1000000);
+  thetaZ = thetaZ + omegaZ * (dty / 1000000);
+  RealAngle = thetaZ ;
 }
